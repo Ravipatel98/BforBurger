caeb281a3ea0956c9baea4a426ebc8c4c5479fe7
Ravipatel98 <patel.ravi.19298@gmail.com>
2021-05-23 14:35:36 -0400

    updated server

--- server/daos/item.dao.js
+++ server/daos/item.dao.js
@@ -3,36 +3,37 @@ const Item = require("../models/item.model");
 const findItems = () => Item.find();
 
 const createItem = (item, itemImage) => {
-  const newItem = new Item({
-    name: item.name,
-    type: item.type,
-    timeToPrep: item.timeToPrep,
-    itemImage,
-  });
-  return newItem.save();
+    const newItem = new Item({
+        name: item.name,
+        type: item.type,
+        timeToPrep: item.timeToPrep,
+        itemImage,
+    });
+    return newItem.save();
 };
 
 const findItemById = (id) => Item.findById(id);
 
-const updateItem = (id, item) => {
-  const updatedItem = {
-    name: item.name,
-    type: item.type,
-    timeToPrep: item.timeToPrep,
-  };
-  return Item.findByIdAndUpdate(id, updatedItem, {
-    useFindAndModify: false,
-  });
+const updateItem = (id, item, itemImage) => {
+    const updatedItem = {
+        name: item.name,
+        type: item.type,
+        timeToPrep: item.timeToPrep,
+        itemImage,
+    };
+    return Item.findByIdAndUpdate(id, updatedItem, {
+        useFindAndModify: false,
+    });
 };
 
 const deleteItem = (id) => Item.findByIdAndDelete(id);
 
 const itemDao = {
-  findItems,
-  createItem,
-  findItemById,
-  updateItem,
-  deleteItem,
+    findItems,
+    createItem,
+    findItemById,
+    updateItem,
+    deleteItem,
 };
 
 module.exports = itemDao;
--- server/routes/item.js
+++ server/routes/item.js
@@ -5,7 +5,7 @@ const upload = require("../fileUploadMW");
 router.get("/", itemController.findItems);
 router.get("/:id", itemController.findItemById);
 router.post("/add", upload.single("p"), itemController.createItem);
-router.put("/update/:id", itemController.updateItem);
+router.put("/update/:id", upload.single("p"), itemController.updateItem);
 router.delete("/delete/:id", itemController.deleteItem);
 
 module.exports = router;
